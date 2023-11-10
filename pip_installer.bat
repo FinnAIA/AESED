607cfd71f5865266b2f3a0cff977e3eeca91a6e5
FinnAIA <144200185+FinnAIA@users.noreply.github.com>
2023-11-10 12:58:31 +0000

    Add files via upload

--- pip_installer.bat
+++ pip_installer.bat
@@ -1,9 +1,7 @@
 @echo off
-title pip_installer.bat
+title pip installer.bat
+py -m pip install --upgrade pip
 py -m pip install pycryptodome
-py -m pip install --upgrade pycryptodome
 py -m pip install tk
-py -m pip install --upgrade tk
-echo INFO -- You can close the program now!
-timeout /t 10 /NOBREAK >nul
+timeout /t 5 /NOBREAK >nul
 exit
